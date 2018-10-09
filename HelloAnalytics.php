62d18a1e09f86f040a36d8d7dad49497265644d0
Sue J. Stevens <SStevens@lc.local>
2018-10-09 15:05:05 -0700

    Initial Files

---
+++ HelloAnalytics.php
@@ -0,0 +1,100 @@
+<?php
+
+// Load the Google API PHP Client Library.
+require_once __DIR__ . '/vendor/autoload.php';
+
+$analytics = initializeAnalytics();
+$response = getReport($analytics);
+printResults($response);
+
+
+/**
+ * Initializes an Analytics Reporting API V4 service object.
+ *
+ * @return An authorized Analytics Reporting API V4 service object.
+ */
+function initializeAnalytics()
+{
+
+  // Use the developers console and download your service account
+  // credentials in JSON format. Place them in this directory or
+  // change the key file location if necessary.
+  //$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/AnalyticsV2/oauth2callback.php';
+  //$KEY_FILE_LOCATION = 'C:\\inetpub\\wwwr...'
+  $KEY_FILE_LOCATION = __DIR__ . '/service-account-credentials.json';
+
+  // Create and configure a new client object.
+  $client = new Google_Client();
+  $client->setApplicationName("Hello Analytics Reporting");
+  $client->setAuthConfig($KEY_FILE_LOCATION);
+  $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
+  $analytics = new Google_Service_AnalyticsReporting($client);
+
+  return $analytics;
+}
+
+
+/**
+ * Queries the Analytics Reporting API V4.
+ *
+ * @param service An authorized Analytics Reporting API V4 service object.
+ * @return The Analytics Reporting API V4 response.
+ */
+function getReport($analytics) {
+
+  // ViewID for Account:  www.rgrdlaw.com 8459946; Property TrackingID: UA-8459946-1; View 92227413; Table ID ga:92227413;
+  $VIEW_ID = "92227413";
+
+  // Create the DateRange object.
+  $dateRange = new Google_Service_AnalyticsReporting_DateRange();
+  $dateRange->setStartDate("7daysAgo");
+  $dateRange->setEndDate("today");
+
+  // Create the Metrics object.
+  $sessions = new Google_Service_AnalyticsReporting_Metric();
+  $sessions->setExpression("ga:sessions");
+  $sessions->setAlias("sessions");
+
+  // Create the ReportRequest object.
+  $request = new Google_Service_AnalyticsReporting_ReportRequest();
+  $request->setViewId($VIEW_ID);
+  $request->setDateRanges($dateRange);
+  $request->setMetrics(array($sessions));
+
+  $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
+  $body->setReportRequests( array( $request) );
+  return $analytics->reports->batchGet( $body );
+}
+
+
+/**
+ * Parses and prints the Analytics Reporting API V4 response.
+ *
+ * @param An Analytics Reporting API V4 response.
+ */
+function printResults($reports) {
+  for ( $reportIndex = 0; $reportIndex < count( $reports ); $reportIndex++ ) {
+    $report = $reports[ $reportIndex ];
+    $header = $report->getColumnHeader();
+    $dimensionHeaders = $header->getDimensions();
+    $metricHeaders = $header->getMetricHeader()->getMetricHeaderEntries();
+    $rows = $report->getData()->getRows();
+
+    for ( $rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
+      $row = $rows[ $rowIndex ];
+      $dimensions = $row->getDimensions();
+      $metrics = $row->getMetrics();
+      for ($i = 0; $i < count($dimensionHeaders) && $i < count($dimensions); $i++) {
+        print($dimensionHeaders[$i] . ": " . $dimensions[$i] . "\n");
+      }
+
+      for ($j = 0; $j < count($metrics); $j++) {
+        $values = $metrics[$j]->getValues();
+        for ($k = 0; $k < count($values); $k++) {
+          $entry = $metricHeaders[$k];
+          print($entry->getName() . ": " . $values[$k] . "\n");
+        }
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# HelloAnalytics
-Simple PHP Application to Test Connection to Google Analytics API
+# Hello Analytics
+A test to get Google Analytics Reporting API v4 up and running
+http://webapps2/HelloWorld/HelloWorld.php
+
+## Versioning Notes:
+* 10/09/2018 initial files started
+* Reference:  https://developers.google.com/analytics/devguides/reporting/core/v4/quickstart/service-php
+
+## Service Account
+ We have a PHP service account with Google Analytics which can be viewed here:
+https://console.developers.google.com/iam-admin/serviceaccounts?project=rgrd-website-analytics
+
+## Service Key 
+The service key provided was downloaded and renamed to:  ‘service-account-credentials.json’.  It resides in the same folder as ‘HelloAnalytics.php’.  The file location can be modified later.
+
+## Service Account Linked to Google Analytics Account
+The service account has been linked to our google analytics account here:
+https://analytics.google.com/analytics/web/#/a8459946w16085079p17041261/admin/suiteusermanagement/account
+
+## Install Composer
+I installed Composer on the WebApps2 server.  You can find Composer here:  https://getcomposer.org/
+
+## Google's Client Library
+https://github.com/googleapis/google-api-php-client
+
+From GitBash I installed the Google’s client library with this command:  composer require google/apiclient:^2.0
+
+## GA Account View
+Inside HelloAnalytics.php I linked our Google Account’s view ID.  
+Here is where to see our account View IDs:  https://ga-dev-tools.appspot.com/account-explorer/
+
+
+## Errors and Issues
+I did encounter a cURL error 60: SSL certificate problem which I assume Jonathan will be able to resolve for us once we are up and running in our new servers.  I solved through a hack of vendor\guzzlehttp\guzzle\src\Handler\CurlFactory.php.  I changed this:
+326: $conf[CURLOPT_SSL_VERIFYHOST] = 2;
+327: $conf[CURLOPT_SSL_VERIFYPEER] = true;
+
+To this:
+
+326: $conf[CURLOPT_SSL_VERIFYHOST] = 0;
+327: $conf[CURLOPT_SSL_VERIFYPEER] = FALSE;
+
+Reference:  https://stackoverflow.com/questions/35638497/curl-error-60-ssl-certificate-prblm-unable-to-get-local-issuer-certificate
+
+  
